3dbd3982e483f250fc5df209eec7e986646dfa2c
caoxinghua <caoxinghua@gomeplus.com>
2018-05-17 10:32:15 +0800

    test

---
+++ FirstIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstIos
+//
+//  Created by caoxinghua on 2018/4/26.
+//  Copyright © 2018年 caoxinghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstIos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FirstIos
+//
+//  Created by caoxinghua on 2018/4/26.
+//  Copyright © 2018年 caoxinghua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the vi·····ew, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
